5e6362d3c8e064066dcdaed2de6ec2fac4a1fafb
Resteban368 <es.rodriguez@udla.edu.co>
2026-05-19 08:03:01 -0500

    debug: retornar error real como JSON para identificar módulo faltante

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const handler = require('../dist/lambda');
-export default handler;
+import type { IncomingMessage, ServerResponse } from 'http';
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const lambda = require('../dist/lambda');
+    await lambda(req, res);
+  } catch (e: any) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      error: e.message ?? String(e),
+      code: e.code,
+      requireStack: e.requireStack ?? [],
+    }));
+  }
+}
